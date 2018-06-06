0aad82902f2a3e04d0fb93d6ab37439712f6d90a
Icebela <xiaoying2708@163.com>
2018-06-06 15:50:21 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>home</h1>
+        <router-link to='/list'>列表页</router-link>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
+
