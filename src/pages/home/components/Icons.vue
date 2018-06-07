2d472c8e25052eb9f0f2391d552cd4b57089c83a
icebale <xiaoying2708@163.com>
2018-06-07 23:18:33 +0800

    icon build

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="icons">
+        <div class="icon"></div>
+        <div class="icon"></div>
+        <div class="icon"></div>
+        <div class="icon"></div>
+        <div class="icon"></div>
+        <div class="icon"></div>
+        <div class="icon"></div>
+        <div class="icon"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"HomeIcons"
+    }
+</script>
+
+<style lang="stylus" scoped>
+  .icons
+    overflow :hidden
+    width:100%
+    height:0
+    padding-bottom:50%
+    background:#eee
+    .icon
+      width:25%
+      float:left
+      padding-bottom:25%
+      background:red
+</style>
